79ebbe1c5d5004d29bf0eb261d428e86e64de06d
Dávid Labanc <david.labanc97@gmail.com>
2025-03-28 12:13:31 +0100

    auth service initial

--- src/app/data/auth/auth.service.ts
+++ src/app/data/auth/auth.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private authTokenKey = 'auth_token';
+  private authStatus = new BehaviorSubject<boolean>(this.hasToken());
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post<{ token: string }>(`${environment.API_URL}/login`, credentials).pipe(
+      tap(response => {
+        localStorage.setItem(this.authTokenKey, response.token);
+        this.authStatus.next(true);
+      })
+    );
+  }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem(this.authTokenKey);
+  }
 }
