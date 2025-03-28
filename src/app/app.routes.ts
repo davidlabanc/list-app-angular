cd4beb77ff4178244f75b298e1707671d56ec867
Dávid Labanc <david.labanc97@gmail.com>
2025-03-28 11:34:33 +0100

    app structure and setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './ui/layouts/login-page/login-page.component';
+import { RegisterPageComponent } from './ui/layouts/register-page/register-page.component';
+import { ContactsPageComponent } from './ui/layouts/contacts-page/contacts-page.component';
+import { ListsPageComponent } from './ui/layouts/lists-page/lists-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'dashboard', component: ContactsPageComponent },
+  { path: 'lists', component: ListsPageComponent },
+  // { path: 'create/contact', component: RegisterPageComponent },
+  // { path: 'create/list', component: RegisterPageComponent },
+];
---
+++ src/app/ui/components/contact-header/contact-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-header.component.html',
+  styleUrl: './contact-header.component.css'
+})
+export class ContactHeaderComponent {
+
+}
