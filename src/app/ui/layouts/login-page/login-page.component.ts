3718bc858eacf2d8ce5fd9ca4b39623a4edb28a6
Dávid Labanc <david.labanc97@gmail.com>
2025-03-28 12:02:22 +0100

    first form

--- src/app/ui/layouts/login-page/login-page.component.ts
+++ src/app/ui/layouts/login-page/login-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../../../data/auth/auth.service'
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  constructor(private authService: AuthService){}
 
+  add(email: string, password: string): void {
+    console.log(email, "password: ", password)
+  }
 }
